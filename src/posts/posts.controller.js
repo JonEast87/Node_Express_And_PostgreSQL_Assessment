74c214c87b3f4fa599934ed9f95aa2a77c161a07
Jon Eastin <picardncce@gmail.com>
2022-07-27 00:00:09 -0700

    finished all tests passing

--- src/posts/posts.controller.js
+++ src/posts/posts.controller.js
@@ -14,17 +14,21 @@ async function postExists(req, res, next) {
 
 async function create(req, res) {
 	// your solution here
-	res.json({ data: '' })
+	res.json({ data: await service.create(req.body.data) })
 }
 
 async function update(req, res) {
 	// your solution here
-	res.json({ data: '' })
+	const updatedPost = {
+		...req.body.data,
+		post_id: req.params.postId,
+	}
+	await service.update(updatedPost).then((data) => res.json({ data: data }))
 }
 
 async function destroy(req, res) {
 	// your solution here
-	res.json({ data: '' })
+	await service.delete(res.locals.post_id).then(() => res.sendStatus(204))
 }
 
 module.exports = {
